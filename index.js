a1ee9e4995629119951b8d779c1c4ef01ee6f602
kengwoh <lee.kengwoh@tm.com.my>
2022-06-22 15:37:32 +0800

    Added support CORS

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require('cors');
 const app = express();
 const port = 3000;
 const programmingLanguagesRouter = require("./routes/programmingLanguages");
 app.use(express.json());
+app.use(cors());
 app.use(
   express.urlencoded({
     extended: true,
